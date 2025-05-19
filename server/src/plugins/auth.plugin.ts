ccf6bf38e23c3e5636b46bb3baaeba90bf811775
Veron Fabian Chetty <veronfc1701@gmail.com>
2025-05-19 10:34:36 +0200

    2.1.3: Middleware to validate tokens and reject invalid/expired ones - Add seperate guest and member JWT plugins and auth cookies
    Fixes #31

--- server/src/plugins/auth.plugin.ts
+++ server/src/plugins/auth.plugin.ts
@@ -1,10 +1,18 @@
 import { jwt } from "@elysiajs/jwt";
-import { Elysia, status } from "elysia";
+import { Elysia } from "elysia";
 
-export const authJwt = new Elysia({ name: "authJwt" }).use(
+export const guestJwt = new Elysia({ name: "guestJwt" }).use(
 	jwt({
-		name: "jwt",
+		name: "guestJwt",
 		secret: Bun.env.JWT_SECRET || "SuperSecret",
-		exp: "1h"
+		exp: "2m"
 	})
 );
+
+export const memberJwt = new Elysia({name: 'memberJwt'}).use(
+  jwt({
+		name: "memberJwt",
+		secret: Bun.env.JWT_SECRET || "SuperSecret",
+		exp: "24h"
+	})
+)
